d449041f1e92ff7c21b2539d4908a78b694e0e32
Bart Thate <bthate67@gmail.com>
2021-06-28 16:09:11 +0200

    remove param

--- test/test_thr.py
+++ test/test_thr.py
@@ -1,21 +1,59 @@
 # This file is placed in the Public Domain.
 
+import ob
+import random
 import unittest
 
-from ob.run import kernel
-from ob.thr import launch
+events = []
+k = ob.run.kernel()
 
-from prm import param, consume, events, exec
-
-k = kernel()
+param = ob.Object()
+param.add = ["test@shell", "bart", ""]
+param.cfg = ["cfg server=localhost", "cfg", ""]
+param.dne = ["test4", ""]
+param.rm = ["reddit", ""]
+param.dpl = ["reddit title,summary,link", ""]
+param.log = ["test1", ""]
+param.flt = ["0", ""]
+param.fnd = ["om.irc.Cfg",
+             "om.log.Log",
+             "om.tdo.Todo",
+             "om.rss.Rss",
+             "om.irc.Cfg server==localhost",
+             "om.rss.Rss rss==reddit rss"]
+param.rss = ["https://www.reddit.com/r/python/.rss"]
+param.tdo = ["test4", ""]
 
 class Test_Threaded(unittest.TestCase):
 
     def test_thrs(self):
         thrs = []
         for x in range(k.cfg.index or 1):
-            thr = launch(exec)
+            thr = ob.launch(exec)
             thrs.append(thr)
         for thr in thrs:
             thr.join()
         consume()
+
+def consume():
+    fixed = []
+    res = []
+    for e in events:
+        e.wait()
+        fixed.append(e)
+    for f in fixed:
+        try:
+            events.remove(f)
+        except ValueError:
+            continue
+    return res
+
+def exec():
+    c = ob.hdl.Bus.first()
+    l = list(k.cmds)
+    random.shuffle(l)
+    for cmd in l:
+        for ex in getattr(param, cmd, [""]):
+            e = c.event(cmd + " " + ex)
+            k.dispatch(e)
+            events.append(e)
